c1928cd92e9e4e11081cfbd03ba14d08ffa40bcc
PennyRen <EmotionalEcho@gmail.com>
2014-12-23 14:30:07 +0800

    .Initial

---
+++ js/MainView.js
@@ -0,0 +1,20 @@
+/**
+ * View: MainView
+ *
+ * Responsibilities:
+ *   - Main Screen for the application.
+ *   - Will create all the necessary sub views
+ *   - Manage all the application wide events
+ */
+(function() {
+	
+	brite.registerView("MainView",{Parent:#pageBody},{
+		
+		create: function(){
+			// 由于第一个View是静态的, 不需要调用 js 渲染, 只需要在模板中用.html() 
+			return render("MainView");
+		}
+			
+	});
+
+})(); 
---
+++ js/main.js
@@ -0,0 +1,23 @@
+// 这里我们应该告诉brite，然后让它去加载... /tmpl/ViewName.tmpl 和.. /css/ViewName.css文件
+//需求中。对于开发非常有用的是，可以关闭生产（如文件可以预编译和链接）。
+brite.viewDefaultConfig.loadTmpl = true;
+brite.viewDefaultConfig.loadCss = true;
+
+// 当文档准备好时，我们显示MainView (同时它将显示子views) 
+$(document).ready(function(){
+  brite.display("MainView","#pageBody");
+});
+
+// 使用handelbars 间接的渲染一个模板
+// 该简单的间接以后允许更多灵活性  
+// 当需要使用预编译或其他模板引擎时
+/*
+Handlebars.templates = Handlebars.templates || {};  
+function render(templateName,data){
+  var tmpl = Handlebars.templates[templateName];
+  if (!tmpl){
+    tmpl = Handlebars.compile($("#" + templateName).html());
+    Handlebars.templates[templateName] = tmpl;
+  }
+  return tmpl(data);
+}
